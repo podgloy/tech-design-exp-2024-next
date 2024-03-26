ebdc9a031af3445939cd94f37d8fa16f133f6538
podgloy <nawarat.saetang@hotmail.com>
2024-03-27 01:03:04 +0700

    UI Development

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Dashboard() {
+    return <div className="text-lg min-h-[100vh]
+    justify-center flex items-center">
+        <a href="" className="p-4 bg-red-500 text-white">back to home</a>
+    </div>;
+}
